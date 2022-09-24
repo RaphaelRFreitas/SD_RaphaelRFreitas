1e7055fae838c0255e337605e4185c909cbb53ba
RaphaelRFreitas <raphael.r@ges.inatel.br>
2022-09-24 13:40:08 -0300

    TodosTestes

--- lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
+++ lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
@@ -46,5 +46,62 @@ class CursoControllerTest {
 			.exchange()
 			.expectStatus().isNotFound();
 	}
+
+	@Test
+	void dadoCursoValido_quandoPostCurso_entaoResponderComCursoCriado() {
+		Curso cursoParaCriar = new Curso();
+		cursoParaCriar.setDescricao("Curso de Teste");
+		cursoParaCriar.setCargaHoraria(100);
+
+		Curso cursoCriado = webTestClient.post().uri("/curso")
+			.contentType(MediaType.APPLICATION_JSON)
+			.bodyValue(cursoParaCriar)
+			.exchange()
+			.expectStatus().isCreated()
+			.expectBody(Curso.class).returnResult().getResponseBody();
+
+		assertNotNull(cursoCriado);
+		assertNotNull(cursoCriado.getId());
+		assertEquals(cursoParaCriar.getDescricao(), cursoCriado.getDescricao());
+		assertEquals(cursoParaCriar.getCargaHoraria(), cursoCriado.getCargaHoraria());
+	}
+
+	@Test
+	void dadoCursoValido_quandoPutCurso_entaoResponderComCursoAtualizado() {
+		Curso cursoParaAtualizar = new Curso();
+		cursoParaAtualizar.setId(1L);
+		cursoParaAtualizar.setDescricao("Curso de Teste");
+		cursoParaAtualizar.setCargaHoraria(100);
+
+		Curso cursoAtualizado = webTestClient.put().uri("/curso")
+			.contentType(MediaType.APPLICATION_JSON)
+			.bodyValue(cursoParaAtualizar)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody(Curso.class).returnResult().getResponseBody();
+
+		assertNotNull(cursoAtualizado);
+		assertEquals(cursoParaAtualizar.getId(), cursoAtualizado.getId());
+		assertEquals(cursoParaAtualizar.getDescricao(), cursoAtualizado.getDescricao());
+		assertEquals(cursoParaAtualizar.getCargaHoraria(), cursoAtualizado.getCargaHoraria());
+	}
+
+	@Test
+	void dadoCursoIdValido_quandoDeleteCurso_entaoResponderComStatusNoContent() {
+		long cursoIdValido = 1L;
+
+		webTestClient.delete().uri("/curso/" + cursoIdValido)
+			.exchange()
+			.expectStatus().isNoContent();
+	}
+
+	@Test
+	void dadoCursoIdInvalido_quandoDeleteCurso_entaoResponderComStatusNotFound() {
+		long cursoIdInvalido = 99L;
+
+		webTestClient.delete().uri("/curso/" + cursoIdInvalido)
+			.exchange()
+			.expectStatus().isNotFound();
+	}
 }
 

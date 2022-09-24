b8bbfa3ce7863925f0bb1b29b9266b4a30b2b637
RaphaelRFreitas <raphael.r@ges.inatel.br>
2022-09-24 13:36:02 -0300

    3Testes

--- lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
+++ lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
@@ -1,8 +1,12 @@
 package br.inatel.labs.labrest.server.controller;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.inatel.labs.labrest.server.model.Curso;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -19,6 +23,28 @@ class CursoControllerTest {
 			.expectStatus().isOk()
 			.expectHeader().contentType(MediaType.APPLICATION_JSON)
 			.expectBody().returnResult();
+	}
+
+	@Test
+	void dadoCursoIdValido_quandoGetCursoPorId_entaoResponderComCursoValido() {
+		long cursoIdValido = 1L;
+
+		Curso cursoRespondido = webTestClient.get().uri("/curso/" + cursoIdValido)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody(Curso.class).returnResult().getResponseBody();
+
+		assertNotNull(cursoRespondido);
+		assertEquals(cursoIdValido, cursoRespondido.getId());
+	}
+
+	@Test
+	void dadoCursoIdInvalido_quandoGetCursoPorId_entaoRespondeComStatusNotFound() {
+		long cursoIdInvalido = 99L;
+
+		webTestClient.get().uri("/curso/" + cursoIdInvalido)
+			.exchange()
+			.expectStatus().isNotFound();
 	}
 }
 

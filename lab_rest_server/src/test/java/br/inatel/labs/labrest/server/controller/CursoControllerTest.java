75b63324942e82c2dd3487bb5838fa8947531b00
RaphaelRFreitas <raphael.r@ges.inatel.br>
2022-09-24 14:02:31 -0300

    AjusteDelete

--- lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
+++ lab_rest_server/src/test/java/br/inatel/labs/labrest/server/controller/CursoControllerTest.java
@@ -6,7 +6,6 @@ import br.inatel.labs.labrest.server.model.Curso;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
